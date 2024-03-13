752ef45aa61fbbbf70601d9c4678e31bde37ec0c
Maxwell Cofie <maxwell@hubtel.com>
2024-03-13 00:30:07 +0000

    worked on the orders layout

--- pages/pos/menu/index.vue
+++ pages/pos/menu/index.vue
@@ -91,10 +91,12 @@
               </div>
             </div>
 
-            <button type="button"
-                    class="focus:outline-none text-white bg-red-500 w-full hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-              Place Order
-            </button>
+            <NuxtLink to="/pos/orders/payment">
+              <button type="button"
+                      class="focus:outline-none text-white bg-red-500 w-full hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                Place Order
+              </button>
+            </NuxtLink>
           </div>
 
         </div>
--- pages/pos/orders/index.vue
+++ pages/pos/orders/index.vue
@@ -1,5 +1,33 @@
 <template>
-    <h1 class="text-4xl">Never</h1>
+  <div class="pt-16">
+    <div class="grid grid-cols-4">
+      <div class="col-span-1 h-screen bg-gray-100">
+
+        <div class="bg-white h-36 rounded-lg m-4 p-3" v-for="i in 5">
+          <div class="flex flex-row justify-between">
+            <div>
+              <h3 class="text-lg">Order Number</h3>
+              <p class="text-gray-500">Something</p>
+              <p class="text-gray-500">Something</p>
+              <p class="text-gray-500">Something</p>
+            </div>
+
+            <div class="flex-col flex text-right">
+              <h2 class="text-lg">GHS 2,039</h2>
+              <div class="flex flex-row space-x-2 my-2">
+                <span
+                    class="bg-blue-100 text-blue-800 text-xs font-medium  px-2.5 py-1.5 rounded-full dark:bg-blue-900 dark:text-blue-300">Unpaid</span>
+                <span
+                    class="bg-yellow-100 text-yellow-800 text-xs font-medium  px-2.5 py-1.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">Cash</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-span-3"></div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
