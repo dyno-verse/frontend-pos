76a26bbb712db5dcd5764da020305403085a55ba
最大 <maxcofie@gmail.com>
2024-03-13 23:46:55 +0000

    Update index.vue

--- pages/kds/index.vue
+++ pages/kds/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="container">
+  <div class="mx-auto">
     <div v-if="!isPending"
-         class="grid grid-cols-5 gap-4 auto-rows-fr items-center justify-items-stretch content-start m-4">
+         class="grid grid-cols-6 gap-4 auto-rows-fr items-center justify-items-stretch content-start m-4">
 
-      <div v-for="(order, index) in orders" class="bg-red-50 w-full h-full rounded-lg border border-red-500 p-5"
-           @dblclick="doSomething()">
+      <div v-if="orders.length !== 0" v-for="(order, index) in orders"
+           class="bg-red-50 w-full h-full rounded-lg border border-red-500 p-5"
+           @dblclick="updateOrderStatus(order.id, index)">
         <h3 class="text-5xl font-extrabold">00{{ order.orderNumber }}</h3>
-        <p class="">Table #2</p>
+        <p class="">Table #{{ order.tableNumber }}</p>
         <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700">
 
         <div v-for="o in order.orderItems">
-          <p class="text-xl my-2">{{ o.item.name }}</p>
+          <p class="text-xl my-2">{{ o.item.name }} (x{{ o.quantity}})</p>
           <p class="my-4 text-gray-500">
-
           </p>
         </div>
 
@@ -30,6 +30,10 @@
         </div>
 
       </div>
+      <div v-else class="col-span-6 mt-12">
+        <EmptyState/>
+      </div>
+
     </div>
     <div class="w-screen text-center h-full py-48" v-else>
       <Loader/>
@@ -38,16 +42,12 @@
 </template>
 
 <script lang="ts" setup>
-import {IOrders, KitchenStatus} from "../../repository/models/ApiResponse";
+import {IKitchenStatus, IOrders, KitchenStatus} from "../../repository/models/ApiResponse";
 
 const branchId = '340328b2-cec0-4c5c-ba57-37a0f33dcf66'
 const snackbar = useSnackbar();
 const orders = ref({} as IOrders)
 
-<<<<<<< HEAD
-import {IOrders} from "../../repository/models/ApiResponse";
-=======
->>>>>>> 50b8a7d6ec53c708fb96d1ab470292256392037c
 
 import Loader from "~/components/units/Loader.vue";
 
@@ -62,25 +62,23 @@ onMounted(() => {
   getAllOrders()
 })
 
-const doSomething = () => {
-  snackbar.add({
-    type: 'success',
-    text: 'Image uploaded'
+
+const updateOrderStatus = (orderId: string, position: number) => {
+  const request: IKitchenStatus = {
+    kitchenStatus: KitchenStatus.PREPARING
+  }
+  orders.value.splice(position, 1)
+
+  $api.order.updateKitchenStatus(orderId, request).then(data => {
+    snackbar.add({
+      type: 'success',
+      text: 'Order moved'
+    })
+  }).catch(error => {
   })
 }
 
 
-<<<<<<< HEAD
-// NOT_STARTED,PREPARING,READY_TO_SERVE
-enum KitchenStatus {
-  NOT_STARTED = 'NOT_STARTED',
-  PREPARING = 'PREPARING',
-  READY_TO_SERVE = 'READY_TO_SERVE'
-}
-
-
-=======
->>>>>>> 50b8a7d6ec53c708fb96d1ab470292256392037c
 const getAllOrders = () => {
   isPending.value = true;
   $api.order.getAllOrders(branchId, KitchenStatus.NOT_STARTED).then(data => {
