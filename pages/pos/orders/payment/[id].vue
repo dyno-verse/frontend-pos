ea6bb58cd6653f8adc9650199040a1018eb77a82
Maxwell Cofie <maxwell@hubtel.com>
2024-03-15 17:00:51 +0000

    ui fixes

--- pages/pos/orders/payment/[id].vue
+++ pages/pos/orders/payment/[id].vue
@@ -51,10 +51,24 @@
              :class="[ selectedPaymentType === type.type ? 'bg-red-100 font-bold': 'bg-gray-100' ]"
              class="p-5 rounded-lg my-2 cursor-pointer flex flex-row space-x-2" v-for="type in paymentMethods">
 
-          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+          <svg v-if="type.type === PaymentType.CASH" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
                width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
                   d="M8 7V6a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1M3 18v-7a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
+          </svg>
+
+          <svg v-if="type.type === PaymentType.MOBILE_MONEY" class="w-6 h-6 text-gray-800 dark:text-white"
+               aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+               viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"/>
+          </svg>
+
+          <svg v-if="type.type === PaymentType.CARD" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"/>
           </svg>
 
           <p>{{ type.name }}</p>
@@ -164,6 +178,7 @@ const paymentMethods = [
 
 const selectPaymentType = (type: PaymentType) => {
   selectedPaymentType.value = type
+  noteSelected.value = '0'
 }
 
 const getOrder = (orderId: string) => {
